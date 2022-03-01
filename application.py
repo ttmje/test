26bf229cdfddbb3b372bbf4c0b1985144eb5ab73
masya <123@gmail.com>
2022-03-01 20:32:04 +0300

    smth for testing

---
+++ application.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def root():
+    return render_template("index.html")
+
+@application.route("/help")
+def helppage():
+    return render_template("help.html")
+
+@application.route("/hello")
+def index():
+    return "My first DEPLOY)))"
+
+#--------Main------------------
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+#------------------------------
